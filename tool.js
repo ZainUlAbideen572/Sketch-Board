e76e83806c2cc4d028f1f8346e2630c2a052aba0
MdZayan <mddzayan572@gmail.com>
2025-05-13 22:28:24 +0530

    resolved changes

--- tool.js
+++ tool.js
@@ -1,137 +1,126 @@
+const canvas = document.querySelector('canvas');
+const pencilwdith = document.querySelector('.pencil-width');
+const pencilcolor = document.querySelectorAll('.pencil-color');
+const eraserwidth = document.querySelector('.eraser-width-cont');
+const undo = document.querySelector('.undo');
+const redo = document.querySelector('.redo');
 
-const canvas=document.querySelector('canvas')
-const pencilwdith=document.querySelector('.pencil-width')
-const pencilcolor=document.querySelectorAll('.pencil-color')
-const eraserwidth=document.querySelector('.eraser-width-cont')
-const undo=document.querySelector('.undo')
-const redo=document.querySelector('.redo')
-canvas.width=window.innerWidth
-canvas.height=window.innerHeight
-let ctx=canvas.getContext("2d")
-let eraserWidthelem=eraserwidth.value
-let currentColor='black'
-ctx.strokeStyle = currentColor; // Set stroke color to green
-let tracker=0
-let undoRedoTracker=[]
-// ctx.beginPath();
-// ctx.moveTo(0, 0); // Starting point of the line
-// ctx.lineTo(200, 200); // Ending point of the line
-// ctx.stroke(); // Render the line
-let mousedown=false
-pencilwdith.addEventListener('change',(e)=>{
-    ctx.lineWidth=e.target.value
-})
-canvas.addEventListener('mousedown',(e)=>{
-    console.log(mousedown)
-    mousedown=true
-        // ctx.beginPath()
-        // ctx.moveTo(e.clientX,e.clientY)
-        let data={
-            x:e.clientX,
-            y:e.clientY
-        }
-        if(mousedown){
-            socket.emit('beginPath',data)
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let ctx = canvas.getContext("2d");
+let eraserWidthelem = eraserwidth.value;
+let currentColor = 'black';
+ctx.strokeStyle = currentColor;
+let tracker = 0;
+let undoRedoTracker = [];
+let mousedown = false;
 
-        }
-})
-function beginPath(data){
-    ctx.beginPath()
-    ctx.moveTo(data.x,data.y)
+pencilwdith.addEventListener('change', (e) => {
+    ctx.lineWidth = e.target.value;
+});
+
+canvas.addEventListener('mousedown', (e) => {
+    console.log(mousedown);
+    mousedown = true;
+    let data = {
+        x: e.clientX,
+        y: e.clientY
+    };
+
+    if (mousedown) {
+        beginPath(data); // Call beginPath directly
+    }
+});
+
+function beginPath(data) {
+    ctx.beginPath();
+    ctx.moveTo(data.x, data.y); // Start the path at the mouse position
 }
-canvas.addEventListener('mousemove',(e)=>{
-    if(mousedown){
-        let data={
-            x:e.clientX,
-            y:e.clientY,
+
+canvas.addEventListener('mousemove', (e) => {
+    if (mousedown) {
+        let data = {
+            x: e.clientX,
+            y: e.clientY,
             color: eraserFlag ? 'white' : currentColor,
-            width: eraserFlag ? eraserWidthelem : pencilwdith
-        }
-        socket.emit('drawStroke',data)
-
+            width: eraserFlag ? eraserWidthelem : pencilwdith.value
+        };
+        drawStroke(data);
     }
-})
-function drawStroke(data){
-    ctx.strokeStyle=data.color,
-    ctx.lineWidth=data.width
-    ctx.lineTo(data.x,data.y)
-    ctx.stroke()
+});
 
+function drawStroke(data) {
+    ctx.strokeStyle = data.color;
+    ctx.lineWidth = data.width;
+    ctx.lineTo(data.x, data.y);
+    ctx.stroke();
 }
-canvas.addEventListener('mouseup',()=>{
-    mousedown=false
-    let url=canvas.toDataURL()
-    undoRedoTracker.push(url)
-    tracker=undoRedoTracker.length-1
-})
-undo.addEventListener('click',(e)=>{
-    if(tracker>0){
-        tracker--
-    }
-    let data={
-        value:tracker,
-        undoRedoTracker:undoRedoTracker
-    }
-    socket.emit('undoRedo',data)
 
-})
-redo.addEventListener('click',(e)=>{
-    if(tracker < undoRedoTracker.length){
-        tracker++
-    }
+canvas.addEventListener('mouseup', () => {
+    mousedown = false;
+    let url = canvas.toDataURL();
+    undoRedoTracker.push(url);
+    tracker = undoRedoTracker.length - 1;
+});
 
-    let data={
-        value:tracker,
-        undoRedoTracker:undoRedoTracker
+// Undo action
+undo.addEventListener('click', () => {
+    if (tracker > 0) {
+        tracker--;
     }
-    socket.emit('undoRedo',data)
+    let data = {
+        value: tracker,
+        undoRedoTracker: undoRedoTracker
+    };
+    undoRedoActions(data);
+});
 
-})
-function undoRedoActions(data){
-     tracker=data.value
-     undoRedoTracker=data.undoRedoTracker
-     let url=undoRedoTracker[tracker]
-    let img=new Image()
-    img.src=url
-    img.onload=(e)=>{
-        ctx.drawImage(img,0,0,canvas.width,canvas.height)
+// Redo action
+redo.addEventListener('click', () => {
+    if (tracker < undoRedoTracker.length) {
+        tracker++;
     }
+    let data = {
+        value: tracker,
+        undoRedoTracker: undoRedoTracker
+    };
+    undoRedoActions(data);
+});
+
+function undoRedoActions(data) {
+    tracker = data.value;
+    undoRedoTracker = data.undoRedoTracker;
+    let url = undoRedoTracker[tracker];
+    let img = new Image();
+    img.src = url;
+    img.onload = (e) => {
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    };
 }
-pencilcolor.forEach((elem)=>{
-   elem.addEventListener('click',()=>{
-    currentColor = elem.classList[0]; // Assuming the class name represents the color
+
+pencilcolor.forEach((elem) => {
+    elem.addEventListener('click', () => {
+        currentColor = elem.classList[0]; // Assuming the class name represents the color
         ctx.strokeStyle = currentColor;
-   })
-})
-eraserwidth.addEventListener('change',(e)=>{
-    eraserwidth.value=e.target.value
-    ctx.lineWidth=eraserWidthelem.value
-})
-eraser.addEventListener('click',()=>{
-    console.log(eraserFlag)
-    if(eraserFlag){
-        ctx.strokeStyle='white'
-        ctx.lineWidth=eraserwidth.value
-    }else{
-        ctx.strokeStyle=currentColor
-        ctx.lineWidth=pencilwdith.value
+    });
+});
+
+eraserwidth.addEventListener('change', (e) => {
+    eraserWidthelem = e.target.value;
+    ctx.lineWidth = eraserWidthelem;
+});
+
+eraser.addEventListener('click', () => {
+    if (eraserFlag) {
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = eraserWidthelem;
+    } else {
+        ctx.strokeStyle = currentColor;
+        ctx.lineWidth = pencilwdith.value;
     }
-})
-function onEraser(data){
-    ctx.strokeStyle='white'
-    ctx.lineWidth=data.value
+});
+
+function onEraser(data) {
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = data.value;
 }
-socket.on('beginPath',(data)=>{
-    beginPath(data)
-})
-socket.on('drawStroke',(data)=>{
-    drawStroke(data)
-})
-
-socket.on('undoRedo',(data)=>{
-    undoRedoActions(data)
-})
-
-
-
-
